14188973ce58eae9f15454fdb5aac564a04daa3c
Theesit Konkaew <theesit@thoughtworks.com>
2016-12-26 13:54:33 +0700

    add hello world path

--- src/main/java/com/eventrock/EventrockApplication.java
+++ src/main/java/com/eventrock/EventrockApplication.java
@@ -2,11 +2,20 @@ package com.eventrock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EventrockApplication {
+
+	@RequestMapping("/")
+	public String home(){
+		return "Hello EventRock!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventrockApplication.class, args);
 	}
+
 }
