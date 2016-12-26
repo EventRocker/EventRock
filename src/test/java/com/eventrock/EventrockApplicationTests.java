de91cdc5b193cc005155623f2c7f90b84edcbfff
Peerapat Junkhaow <junkhaow@thoughtworks.com>
2016-12-26 10:45:35 +0700

    initial project (SpringBoot)

---
+++ src/test/java/com/eventrock/EventrockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eventrock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventrockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
