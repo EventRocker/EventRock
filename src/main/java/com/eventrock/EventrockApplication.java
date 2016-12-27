58fb8cd4373ac464abfbf21536ddaa86ee03c48f
Theesit Konkaew <theesit@thoughtworks.com>
2016-12-27 14:06:49 +0700

    add liquibase and create user

--- src/main/java/com/eventrock/EventrockApplication.java
+++ src/main/java/com/eventrock/EventrockApplication.java
@@ -1,7 +1,10 @@
 package com.eventrock;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
