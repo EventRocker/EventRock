84b5d25900f893eadd78f53bdd67c453467792e8
amornthip <arakkwam@thoughtworks.com>
2016-12-27 14:50:01 +0700

    [Create Event] add thymeleaf-extras-java8time for showing date in thymeleaf

--- src/main/java/com/eventrock/config/ThymeleafConfiguration.java
+++ src/main/java/com/eventrock/config/ThymeleafConfiguration.java
@@ -2,6 +2,8 @@ package com.eventrock.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 
@@ -18,4 +20,17 @@ public class ThymeleafConfiguration {
         webTemplateResolver.setCacheable(false);
         return webTemplateResolver;
     }
+
+    @Bean
+    public TemplateEngine templateEngine(){
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.addDialect(new Java8TimeDialect());
+        return templateEngine;
+    }
+
+    @Bean
+    public Java8TimeDialect java8TimeDialect() {
+        return new Java8TimeDialect();
+    }
+
 }
---
+++ src/main/java/com/eventrock/model/Event.java
@@ -0,0 +1,7 @@
+package com.eventrock.model;
+
+/**
+ * Created by amornthip on 12/27/2016 AD.
+ */
+public class Event {
+}
