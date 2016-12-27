cbe2021c2326f70b5288d9f6a395021d3140aad3
Peerapat Junkhaow <junkhaow@thoughtworks.com>
2016-12-27 14:48:02 +0700

    [Docker] change local-db.sh

--- infrastructure/local-db.sh
+++ infrastructure/local-db.sh
@@ -1,1 +1,1 @@
-docker run -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=mysecretpassword -e POSTGRES_DB=eventrock --name postgres -d postgres
+docker run -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=mysecretpassword -e POSTGRES_DB=eventrock --name postgres -d postgres
