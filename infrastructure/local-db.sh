9d89751e4b8c12507aa49162c3f77b3b4b01c4d9
Salah Chalermthai <schalerm@thoughtworks.com>
2016-12-28 15:59:23 +0700

    migrate from postgres to mysql

--- infrastructure/local-db.sh
+++ infrastructure/local-db.sh
@@ -1,1 +1,9 @@
-docker run -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=mysecretpassword -e POSTGRES_DB=eventrock --name postgres -d postgres
+#!/bin/bash
+
+# make sure we are going to execute docker-compose at the gateway folder
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+docker stop mysql || true
+docker-compose -f mysql-docker-compose.yml up -d --build
